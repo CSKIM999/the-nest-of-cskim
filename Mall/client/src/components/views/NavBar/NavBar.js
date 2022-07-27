a03250d4593465516fc154faaf5791ebe60be1a0
CSKIM999 <75712211+CSKIM999@users.noreply.github.com>
2022-07-27 23:12:47 +0900

    22/07/27 simple change . . .
    23:12 from desktop

--- Mall/client/src/components/views/NavBar/NavBar.js
+++ Mall/client/src/components/views/NavBar/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import LeftMenu from './Sections/LeftMenu';
 import RightMenu from './Sections/RightMenu';
+import { DingdingOutlined } from '@ant-design/icons'
 import {  Menu } from 'antd';
 import './Sections/Navbar.css';
 
 
 function NavBar() {
-  const item = [{label : (<a href="/">Logo</a>)}]
+  const item = [{label : (<DingdingOutlined style={{fontSize:'1.5rem'}}  href="/" />)},{label : (<a href="/">Logo</a>)}]
   const RightMenuItem = RightMenu().items
   const LeftMenuItem = LeftMenu().items
   return <Menu mode='horizontal' items = {item.concat(LeftMenuItem,RightMenuItem)} />
--- Mall/client/src/components/views/RegisterPage/RegisterPage.js
+++ Mall/client/src/components/views/RegisterPage/RegisterPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import {useDispatch} from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { registerUser } from '../../../_actions/user_action'
+import {Button, Form, Input } from 'antd'
 
 function RegisterPage(props) {
   const dispatch = useDispatch();
@@ -26,9 +27,11 @@ function RegisterPage(props) {
 
 
   const onSubmitHandler = (event) => {
+
     event.preventDefault();
     // submit event 이기때문에 이 이 페이지는 제출되나, 이동시키는 페이지가 없어
     // 페이지가 새로고침 되기만 함.
+    // 추가로 antd 를 사용하므로 밑의 검증과정은 필요없지만 있어서 나쁠건 없으니 그대로 가져감
     if (Password !== ConfirmPassword) {
       return alert('비밀번호가 일치하지 않습니다')
     }
@@ -53,7 +56,7 @@ function RegisterPage(props) {
       display:'flex', justifyContent: 'center', alignItems:'center'
       , width:'100%', height:'100%'
     }}>
-      <form style={{display:'flex', flexDirection:'column'}}
+      {/* <form style={{display:'flex', flexDirection:'column'}}
         onSubmit={onSubmitHandler}
       >
 
@@ -70,8 +73,71 @@ function RegisterPage(props) {
         <input type="password" value={Password} onChange={onPasswordHandler} />
         
         <button type='submit'>회원가입</button>
-      </form>
+      </form> */}
 
+      <Form name="register" onFinish={onSubmitHandler}>
+        <Form.Item name='email' label="E-mail" rules={[
+          {
+            type:'email',
+            message:'Please Check Your Valid E-mail'
+          },
+          {
+            required:true,
+            message:'Please input your E-mail'
+          }
+        ]}>
+          <Input onChange={onEmailHandler}/>
+        </Form.Item>
+        <Form.Item name='name' label="Name" rules={[
+          {
+            type:'Name',
+            message:'Please Check Your Name'
+          },
+          {
+            required:true,
+            message:'Please input your Name'
+          }
+        ]}>
+          <Input onChange={onNameHandler}/>
+        </Form.Item>
+        <Form.Item name='password' label="Password" rules={[
+          {
+            type:'password',
+            message:'Please Check Valid Your Password'
+          },
+          {
+            required:true,
+            message:'Please input your Password'
+          }
+        ]}>
+          <Input.Password onChange={onPasswordHandler}/>
+        </Form.Item>
+        <Form.Item name='confirm' label="P/W Check" rules={[
+          {
+            type:'confirm',
+            message:'Please Check Valid Your Password'
+          },
+          {
+            required:true,
+            message:'Please input your Password Again'
+          },
+          ({getFieldValue}) => ({
+            validator(_,value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error('Password is not Match'))
+            }
+          })
+        ]}>
+          <Input.Password onChange={onConfirmPasswordHandler}/>
+        </Form.Item>
+        <Form.Item onSubmit={onSubmitHandler}>
+          <Button type="primary" htmlType="submit" >
+            Regist
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   )
 }
